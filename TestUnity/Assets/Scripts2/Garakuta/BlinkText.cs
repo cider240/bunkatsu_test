73252a690e6c97269754a7606b582d8247f43d3f
nunoura <phill_sls@outlook.com>
2018-07-29 15:18:05 +0900

    aaaaa

---
+++ TestUnity/Assets/Scripts2/Garakuta/BlinkText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkText : MonoBehaviour
+{
+    [SerializeField]
+    Text blinkText;
+
+    [SerializeField]
+    float blinkInterval;
+    
+
+    float lastTime;
+
+    void Awake()
+    {
+  	  // test
+        lastTime = Time.time;
+    }
+
+    void Update()
+    {
+        if((Time.time - lastTime) > blinkInterval)
+        {
+            lastTime = Time.time;
+            blinkText.enabled = !blinkText.enabled;
+        }
+    }
+}
